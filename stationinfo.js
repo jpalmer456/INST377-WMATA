b9864bcb490f9583c4135197e9c7816a3fe5e560
kennethhill007 <153231196+kennethhill007@users.noreply.github.com>
2023-12-07 15:55:06 -0500

    Add files via upload

--- stationinfo.js
+++ stationinfo.js
@@ -1,13 +1,27 @@
 function stationInfo() {
-    console.log("You're in the function")
+    console.log("content loaded");
 
-    const metroKey = "327336c3b459437b953cfabe8b3583c3"
+    var params = {
+        "api_key": "327336c3b459437b953cfabe8b3583c3",
+        /*"LineCode": "{string}",*/
+    };
 
-    fetch("https://api.wmata.com/Rail.svc/json/jStations/" + metroKey)
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res)
+    $.ajax({
+        url: "https://api.wmata.com/Rail.svc/json/jStations?" + $.param(params),
+        type: "GET",
+    })
+    .done(function(data) {
+        //alert("success");
+        var select = '<option disabled selected>Select a Station:</option>';
+        var stations = data.Stations;
+        $.each(stations, function(i, item) {
+            select += '<option value="'+item.Name+'">'+ item.Name +'</option>';
         });
+        $("#station").html(select);
+    })
+    .fail(function() {
+        alert("error");
+    });
 }
 
 window.onload = stationInfo;
